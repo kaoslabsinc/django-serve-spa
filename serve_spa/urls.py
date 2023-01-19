bdc7869747f01c582d1eaa9a69bf1137c717e766
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-01-19 16:31:55 -0500

    Document and comment code

--- serve_spa/urls.py
+++ serve_spa/urls.py
@@ -18,8 +18,10 @@ class SPAFilenameConverter:
 register_converter(SPAFilenameConverter, 'spa_filename')
 
 urlpatterns = [
-    path('static/<path:resource>', views.redirect_static),
-    path('<spa_filename:filename>', views.serve_file),
+    path('static/<path:resource>', views.redirect_static),  # Serve SPA static files using django staticfiles
+    path('<spa_filename:filename>', views.serve_file),  # Serve other SPA file like manifest.json
+    # Serve SPA paths from the index.html entrypoint.
     path('<path:resource>/', views.serve_file, {'filename': 'index.html'}, name='path'),
+    # Serve SPA home from the index.html entrypoint.
     path('', views.serve_file, {'filename': 'index.html'}, name='home'),
 ]
